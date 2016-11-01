5650b908ce6c9f909e8ab42725cc32384da722e8
Jack Nelson <jwnelsoniv@gmail.com>
2016-10-31 19:01:54 -0700

    Binary huffman encoder now works. English binary huffman encoding in English.out. Added some command line UI options. Now to generalize to r-ary...

--- HuffmanEncoder/huffman.py
+++ HuffmanEncoder/huffman.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 import sys
-import numpy
 import math
+import collections
 
 class HuffmanTree():
     
-    def __init__(self, radix, root_node):
+    def __init__(self, radix, root_node, verbose = False):
         """
             Initialize a tree
         """
@@ -12,28 +13,41 @@ class HuffmanTree():
         self.root_node = root_node
         self.nodes = []
         self.encoding = {}
+        self.verbose = verbose
 
     def build_huffman_tree(self, huffman_nodes, level = 0):
         """
             Recursive Huffman function to build the entire Huffman tree based on a list of HuffmanNodes and their probabilities.
         """
-        print("HuffmantTree.huffman_encode()")
+        if self.verbose:
+            print("\nHuffmanTree.huffman_encode()")
         # sort the list of HuffmanNodes by their weights from least to greatest
         huffman_nodes = sorted(huffman_nodes, key = lambda node: node.weight)
 
-        while len(huffman_nodes) > 1:
-            print("len(huffman_nodes) = %d" %len(huffman_nodes))
+        if len(huffman_nodes) > 1:
+            if self.verbose:
+                print("huffman_nodes: ", [node.weight for node in huffman_nodes])
+                print("len(huffman_nodes) = %d" %len(huffman_nodes))
+                if len(huffman_nodes) == 2:
+                    print("We should stop after this iteration")
 
             # pop the two lowest weight nodes (symbols) from the list of nodes.
             children = []
             for n in range(0,2):
                 children.append(huffman_nodes.pop(0))
+
+            if self.verbose:
+                print("huffman_nodes after popping children: ", [node.weight for node in huffman_nodes])
+                print("nodes in children list: ", [node.weight for node in children])
+            
+                
             
             # make an empty parent node
             parent_weight = sum(child.weight for child in children)
-            parent_node = HuffmanNode(symbol = "%s_%1.2f" %(level, parent_weight), weight = parent_weight, parent = None, children = children, level = level)
+            parent_node = HuffmanNode(symbol = "%s_%1.2f" %(level, parent_weight), weight = parent_weight, parent = None, children = children, level = level + 1)
             self.nodes.append(parent_node)
-            print(parent_node.level)
+            if self.verbose:
+                print("Parent node level: ", parent_node.level)
 
             # Set the children node's pointers to point to their parent, and 
             self.add_children(parent_node, children)
@@ -54,14 +68,17 @@ class HuffmanTree():
 
     def add_children(self, parent, children):
         """
-            Add a children nodes to a parent node. Simply adjusts the pointers
-            of the respective parent and child nodes.
+            Add children nodes to a parent node. Simply adjusts the parent pointers
+            of the child nodes to point to the parent.
+
+            parent - A single HuffmanNode object.
+            children - An iterable of HuffmanNode objects.
         """
-        print("Adding children")
-        print children
+        if self.verbose:
+            print("Adding children")
         for child in children:
             child.parent = parent
-            child.level = parent.level + 1
+            child.level = parent.level - 1  # 0 is the bottom of the tree
         return
 
     def traverse_and_callback(self, start_node, callback = None):
@@ -98,7 +115,8 @@ class HuffmanTree():
             if node.parent is not None:
                 node.code = ''.join(str(e) for e in node.code)
                 self.encoding[node.symbol] = node.code
-                print("%s: %s" %(node.symbol, node.code))
+                if self.verbose:
+                    print("%s: %s" %(node.symbol, node.code))
 
         #print("Out " + node.symbol)
         return
@@ -134,14 +152,32 @@ class HuffmanNode():
             return False
 
 class HuffmanEncoder():
-    def __init__(self, input_file):
+    def __init__(self, argv):
+
+        self.input_file = argv[0]
+        self.verbose = False
+        self.output_file = None
+        self.radix = 2
+
+        if '-v' in argv:
+            self.verbose = True
+
+        if '-o' in argv:
+            self.output_file = argv[argv.index('-o') + 1]
+
+        if '-r' in argv:
+            self.radix = argv[argv.index('-r') + 1]
+
         # Read the problem definition into the dict symbols
-        symbols = self._read_problem(input_file[0]) #TODO - replace passing in filename with file prompt
+        symbols = self._read_problem(self.input_file) #TODO - replace passing in filename with file prompt
         if symbols == -1:
             print("Exiting Program")
             return
         else:
-            print symbols
+            if self.verbose:
+                print("Symbols and weights:")
+                for symbol in symbols:
+                    print("%s: %f" %(symbol, symbols[symbol]))
 
         # create a list of sorted nodes from the symbols and their weights
         self.huffman_nodes = self._create_nodes(symbols)
@@ -150,14 +186,13 @@ class HuffmanEncoder():
 
 
 
-    def _read_problem(self, infile, radix = 2):
+    def _read_problem(self, infile):
         """
             Read in a Huffman encoding problem from a text file. Return a 
             dict keyed on the symbols with the symbol probabilities (weights)
             as the values.
         """
         symbols = {}
-        self.radix = radix
         with open(infile, 'r') as f:
             for line in f:
                 line = line.rstrip('\n').split(',')
@@ -197,9 +232,13 @@ class HuffmanEncoder():
         """
             Perform the Huffman encoding algorithm using symbols read in as huffman_nodes.
         """
-        print("Running Huffman encoding")
+        print("\nRunning Huffman encoding")
+        print("Infile: %s\nRadix: %d" %(self.input_file, self.radix))
+        if self.output_file:
+            print("Output: %s" %self.output_file)
+
         # create a HuffmanTree object to store the tree relations of the HuffmanNodes
-        huff_tree = HuffmanTree(radix = 2, root_node = None)
+        huff_tree = HuffmanTree(radix = self.radix, root_node = None, verbose = self.verbose)
 
         # build the huffman tree
         huff_tree.build_huffman_tree(self.huffman_nodes)
@@ -207,15 +246,30 @@ class HuffmanEncoder():
         # assign codewords to the nodes in the huffman tree
         huff_tree.binary_traverse_and_encode(huff_tree.root_node)
 
-        # traverse the tree we just created
-        """
-        print(huff_tree.root_node)
-        print("\tSymbol\t|\tWeight\t|\tLevel\t|\tParent\t|")
-        huff_tree.traverse_and_callback(huff_tree.root_node)
-        """
+        # sort the encoding alphabetically by symbol
+        huff_tree.encoding = collections.OrderedDict(sorted(huff_tree.encoding.items()))
+        
+        if self.output_file is None or self.verbose is True:
+            print("\nCode:")
+            for symbol in huff_tree.encoding:
+                print("%s: %s" %(symbol, huff_tree.encoding[symbol]))
+        if self.output_file is not None:
+            # save the encoding to the given file
+
+            with open(self.output_file, 'w') as of:
+                for symbol in huff_tree.encoding:
+                    of.write("%s, %s\n" %(symbol, huff_tree.encoding[symbol]))
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
+        print("""
+            Usage: huffman.py symbol_file [-o output_file] [-r radix] [-v]\n
+                \tsymbol_file: File containing the symbols and associated weights (probabilities) in csv format.\n
+                \t-o output_file: Put the encoding output into output_file in csv format.\n
+                \t-r radix: Encode using the given radix. Defaults to radix = 2 (binary).\n
+                \t-v: Verbose mode. Prints out a lot of stuff.\n
+            """)
+
         print("Must pass a filename containing smybols and their frequency.")
     else:
         encoder = HuffmanEncoder(sys.argv[1:])
